244769b3f93ca2244052e64d2f83cd9954da92c8
Max <max@ehlers.berlin>
2018-09-21 11:40:57 +0200

    sets up new mq tests

--- test/export-properties.mjs
+++ test/export-properties.mjs
@@ -2,5 +2,6 @@ export const customProperties = {
 	'--ref-color': 'var(--color)',
 	'--color': 'rgb(255, 0, 0)',
 	'--circular': 'var(--circular-2)',
-	'--circular-2': 'var(--circular)'
+	'--circular-2': 'var(--circular)',
+	'mediaQueries': ''
 };
